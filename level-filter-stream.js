c14a77b3d5174b31d5599b8ae51e09c3531cbcf0
Rod Vagg <rod@vagg.org>
2013-05-05 14:16:51 +1000

    make smarter

--- level-filter-stream.js
+++ level-filter-stream.js
@@ -1,19 +1,39 @@
 const TransformStream  = require('stream').Transform
     , inherits         = require('util').inherits
 
+function inArray (arr, test) {
+  var i = 0
+    , testElement = test instanceof RegExp
+        ? function (el) { return test.test(el) }
+        : function (el) { return test.indexOf(el) > -1 } // test = array
+
+  for (; Array.isArray(arr) && i < arr.length; i++)
+    if (testElement(arr[i]))
+      return true
+
+  return false
+}
+
 function isLevelDB (pkg) {
   if (!pkg)
     return false
 
-  if (pkg.name && /level(-|up|down|db)/.test(pkg.name))
+  if (pkg.name && /level(-|up|down|db|\.?js)/.test(pkg.name))
     return true
+
   var v = pkg['dist-tags'] && pkg['dist-tags'].latest
-    , i
-  if (v && pkg.versions && pkg.versions[v] && Array.isArray(pkg.versions[v].keywords)) {
-    for (i = 0; i < pkg.versions[v].keywords.length; i++)
-      if (/level(up|down|db)/.test(pkg.versions[v].keywords[i]))
-        return true
-  }
+
+  if (!v || !pkg.versions || !pkg.versions[v])
+    return false
+
+  if (inArray(pkg.versions[v].keywords, (/level(up|down|db|\.?js)/g)))
+    return true
+
+  if (inArray(
+        Object.keys(pkg.versions[v].dependencies || {})
+      , [ 'levelup', 'leveldown', 'level.js', 'abstract-leveldown' ]))
+    return true
+
   return false
 }
 
@@ -30,3 +50,4 @@ LevelFilterStream.prototype._transform = function(chunk, encoding, callback) {
 }
 
 module.exports = LevelFilterStream
+module.exports.isLevelDB = isLevelDB
---
+++ test.js
@@ -0,0 +1,23 @@
+var assert    = require('assert')
+  , isLevelDB = require('./level-filter-stream').isLevelDB
+
+assert(!isLevelDB({}))
+assert(isLevelDB({ name: 'levelup' }))
+assert(isLevelDB({ name: 'leveldown' }))
+assert(isLevelDB({ name: 'level.js' }))
+assert(isLevelDB({ name: 'leveljs' }))
+assert(isLevelDB({ name: 'abstract-leveldown' }))
+
+assert(isLevelDB({ 'dist-tags': { latest: 'foo' }, versions: { 'foo': { keywords: [ 'level.js' ]}}}))
+assert(isLevelDB({ 'dist-tags': { latest: 'foo' }, versions: { 'foo': { keywords: [ 'leveldb' ]}}}))
+assert(isLevelDB({ 'dist-tags': { latest: 'foo' }, versions: { 'foo': { keywords: [ 'levelup' ]}}}))
+assert(isLevelDB({ 'dist-tags': { latest: 'foo' }, versions: { 'foo': { keywords: [ 'leveldown' ]}}}))
+assert(isLevelDB({ 'dist-tags': { latest: 'foo' }, versions: { 'foo': { keywords: [ 'awesome leveldb sauce' ]}}}))
+assert(!isLevelDB({ 'dist-tags': { latest: 'foo' }, versions: { 'foo': { keywords: [ 'bar', 'bang' ]}}}))
+
+assert(isLevelDB({ 'dist-tags': { latest: 'foo' }, versions: { 'foo': { dependencies: { levelup: '*' }}}}))
+assert(isLevelDB({ 'dist-tags': { latest: 'foo' }, versions: { 'foo': { dependencies: { 'level.js': '*' }}}}))
+assert(isLevelDB({ 'dist-tags': { latest: 'foo' }, versions: { 'foo': { dependencies: { leveldown: '*' }}}}))
+assert(isLevelDB({ 'dist-tags': { latest: 'foo' }, versions: { 'foo': { dependencies: { 'bar': '*', 'abstract-leveldown': '*' }}}}))
+assert(!isLevelDB({ 'dist-tags': { latest: 'foo' }, versions: { 'foo': { }}}))
+assert(!isLevelDB({ 'dist-tags': { latest: 'foo' }, versions: { 'foo': { dependencies: { levelnot: '*' }}}}))
