bd4eaf60719eaaaafbe95dfe8f552f01e4838959
Rod Vagg <rod@vagg.org>
2013-06-20 13:01:06 +1000

    redirect users in #leveldb to ##leveldb

--- level-bot.js
+++ level-bot.js
@@ -1,13 +1,17 @@
 const NpmPublishStream  = require('npm-publish-stream')
     , jerk              = require('jerk')
+    , fs                = require('fs')
+    , path              = require('path')
     , LevelFilterStream = require('./level-filter-stream')
     , options           = require('./options')
+
+    , dataStore         = path.join(process.env.HOME, '.levelbot.json')
 
 /* options.json needs to be like this:
 {
     "server"     : "irc.freenode.net"
   , "nick"       : "<some username>"
-  , "channels"   : [ "##leveldb" ]
+  , "channels"   : [ "##leveldb", "#leveldb" ]
   , "password"   : "<some password>"
 }
 */
@@ -38,5 +42,40 @@ function setup () {
   streaming = true
 }
 
+// Sync, cause it's easy and this isn't very often
+function redirectUser (channel, user) {
+  if (channel != options.redirectChannel)
+    return
+
+  var data = fs.readFileSync(dataStore, 'utf8')
+  data = JSON.parse(data)
+  if (!data.redirectedUsers)
+    data.redirectedUsers = []
+
+  if (data.redirectedUsers.indexOf(user) > -1)
+    return
+
+  bot.say(
+      channel
+    ,   user
+      + ': not much happening in here, /join ##leveldb, that\'s where the action is!'
+  )
+  data.redirectedUsers.push(user)
+
+  fs.writeFileSync(dataStore, JSON.stringify(data, null, 2), 'utf8', function () {})
+}
+
 options.onConnect = function () { setTimeout(setup, 5000) }
-bot = jerk(function (j) { /* TODO: something fun... */ }).connect(options)
+bot = jerk(function (j) {
+  options.redirectChannel && j.watch_for('.*', function (msg) {
+    redirectUser(msg.source.toString(), msg.user)
+  })
+
+  options.redirectChannel && j.user_join(function (msg) {
+    redirectUser(msg.text[0], msg.user)
+  })
+}).connect(options)
+
+if (!fs.existsSync(dataStore)) {
+  fs.writeFileSync(dataStore, '{}', 'utf8')
+}
